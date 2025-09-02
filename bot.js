53f032addd3dfdd5896ce3ef2d294fbb4600ea38
jpfagundes <jpsafagundes@hotmail.com>
2025-09-02 09:51:24 -0300

    fix: fluxo do bot

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ async function coletarRecompensa() {
 
   // 👉 Login
     console.log("🔹 Iniciando login...");
-    await page.locator('.van-popup > div > div > div:nth-child(2) > img').click();
+    // await page.locator('.van-popup > div > div > div:nth-child(2) > img').click();
     await page.locator('.app-name-leftbg > img').click();
     await page.locator('.loginreg-frame-bg').click();
     await page.getByText('Entrar').nth(4).click();
